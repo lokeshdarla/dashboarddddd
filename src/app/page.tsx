174e75d7763b3059bd20de29ca7d638b92195230
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-19 21:24:28 +0530

    feat : add sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
-  <h1>Hello World</h1>
+  <Sidebar/>
   );
 }
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,83 @@
+'use client'
+import { useState } from "react";
+import { FaCircleArrowLeft } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+import { RiPieChart2Fill } from "react-icons/ri";
+import { MdLibraryBooks, MdNavigateBefore, MdNavigateNext } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import Image from "next/image";
+import { BsFire } from "react-icons/bs";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { MdOutlineNavigateNext } from "react-icons/md";
+
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+
+  const Menus = [
+    { title: "Home", icon: <FaHome /> },
+    { title: "Notes", icon: <MdLibraryBooks /> },
+    { title: "Statistics", icon: <RiPieChart2Fill/>},
+    { title: "Chat", icon:<IoChatboxEllipsesOutline />},
+    { title: "Settings",icon:<IoSettingsOutline/>}
+  ];
+
+const toggleSideBar=()=>{
+  setOpen(!open);
+}
+
+  return (
+    <div className="flex">
+      <div
+        className={` ${
+          open ? "w-60" : "w-18"
+        } bg-[#111729] h-screen relative duration-300`}
+      >
+        <FaCircleArrowLeft
+              className={`absolute cursor-pointer -right-2 border border-black top-28 rounded-full text-3xl ${!open && "rotate-180"} text-white`}
+              onClick={toggleSideBar}
+            />
+        <div className="flex h-screen items-center flex-col justify-around">
+          <BsFire className="text-[#409BEE]" size={30}/>
+          <ul className="flex flex-col items-start gap-5 w-full justify-center px-3">
+          {Menus.map((Menu, index) => (
+            <li
+              key={index}
+              className={`flex rounded-md px-3 py-3 cursor-pointer hover:bg-[#2C3A58]  text-[#CCCCCC] items-center justify-center gap-10 
+                ${index === 0 && "bg-[#2C3A58] "} ${open && "w-full"} `}
+            >
+              <span className="text-xl">{Menu.icon}</span>
+              <span className={`${!open && "hidden"} origin-left duration-200 flex items-center justify-between w-full`}>
+                {Menu.title}
+                <MdNavigateNext/>
+              </span>
+            </li>
+          ))}
+          </ul>
+          <div className="flex-col flex items-center justify-center gap-5">
+          <div className="flex items-center justify-center gap-5">
+          <Image
+            src="/user.png"
+            width={50}
+            height={50}
+            alt="Picture of the author"
+          /> <span className={`${!open && "hidden"} origin-left duration-200 text-white`}>
+          Lokesh
+          </span>
+          </div>
+        
+         
+          <FaArrowRightFromBracket size={20} className="text-[#CCCCCC]"/>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 h-screen p-7 ">
+        <h1 className="text-2xl font-semibold">Home Page</h1>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
